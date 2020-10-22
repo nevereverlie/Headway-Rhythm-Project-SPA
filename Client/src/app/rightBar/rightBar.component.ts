b7b86fa02bbfc7542a3918c470997481e0427aab
Hladkov Danylo <danylo.hladkov@nure.ua>
2020-10-23 00:29:22 +0300

    Minor quality web changes Filter bar colors improved Clear filers button added RightBar enhanced with real genres

--- Client/src/app/rightBar/rightBar.component.ts
+++ Client/src/app/rightBar/rightBar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreService } from '../_services/genre.service';
 
 @Component({
   selector: 'app-rightBar',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rightBar.component.css']
 })
 export class RightBarComponent implements OnInit {
-
-  constructor() { }
+  genres: any;
+  constructor(public genreService: GenreService) { }
 
   ngOnInit() {
+    this.getGenres();
+    console.log(this.genres);
   }
 
+  getGenres(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.genreService.getGenres().subscribe(
+        (genres) => {
+          resolve(this.genres = genres);
+        },
+        (error) => {
+          reject(console.log(error));
+        }
+      );
+    });
+  }
 }
